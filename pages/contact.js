0b77d21ca080abb331ea4d3d2b50e6f35ad34cdb
surendrainnamuri <surendrainnamuri@gmail.com>
2026-04-25 21:46:40 +0100

    Updated contact.js with complete contact information

---
+++ pages/contact.js
@@ -0,0 +1,19 @@
+const contactInfo = {
+  addresses: {
+    Bangalore: {
+      address: "Suite #9, 3rd Floor, Gamma Tower, Sigma Soft Tech Park, Whitefield, Bangalore – 560066",
+      phone: "+91 80 8801 5575"
+    },
+    Hyderabad: {
+      address: "[Add Hyderabad Address Here]",
+      phone: "+91 [Add Phone Number Here]"
+    },
+    Chennai: {
+      address: "[Add Chennai Address Here]",
+      phone: "+91 [Add Phone Number Here]"
+    }
+  },
+  email: "info@webstem.in"
+};
+
+export default contactInfo;
