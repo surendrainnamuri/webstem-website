879bcc5039b95cb70e5b6c3c6cf3c007093df40c
surendrainnamuri <surendrainnamuri@gmail.com>
2026-04-25 21:47:10 +0100

    Update Footer.jsx to include all office locations

---
+++ components/Footer.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <div>
+                <h3>Our Office Locations</h3>
+                <ul>
+                    <li>
+                        <strong>Bangalore</strong><br />
+                        Suite #9, 3rd Floor,<br />
+                        Gamma Tower,<br />
+                        Sigma Soft Tech Park,<br />
+                        Whitefield, Bangalore – 560066<br />
+                        Phone: +91 80 8801 5575<br />
+                        Email: <a href='mailto:info@webstem.in'>info@webstem.in</a>
+                    </li>
+                    <li>
+                        <strong>Hyderabad</strong><br />
+                        Suite #9, 3rd Floor,<br />
+                        Gamma Tower,<br />
+                        Sigma Soft Tech Park,<br />
+                        Whitefield, Hyderabad – 560066<br />
+                        Phone: +91 80 8801 5575<br />
+                        Email: <a href='mailto:info@webstem.in'>info@webstem.in</a>
+                    </li>
+                    <li>
+                        <strong>Chennai</strong><br />
+                        Suite #9, 3rd Floor,<br />
+                        Gamma Tower,<br />
+                        Sigma Soft Tech Park,<br />
+                        Whitefield, Chennai – 560066<br />
+                        Phone: +91 80 8801 5575<br />
+                        Email: <a href='mailto:info@webstem.in'>info@webstem.in</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
